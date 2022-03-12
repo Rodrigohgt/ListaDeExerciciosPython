6aa09f24a144fd15d1062bd98ee62ae97861c4b9
Rodrigoohgt <65993267+Rodrigohgt@users.noreply.github.com>
2022-03-12 06:55:30 -0300

    commitmodified

--- adivinharfor.py
+++ adivinharfor.py
@@ -4,8 +4,9 @@ print("Jogo de adivinhacao")
 tentativa = 0
 num_secreto = random.randrange(1,51)
 rodada = 1
+pontos = 500
 
-dificuldade = int(input("Selecione o nivel de dificuldade, Facil: 1 / Médio: 2 / Dificil: 3"))
+dificuldade = int(input("Selecione o nivel de dificuldade, Facil: 1 / Médio: 2 / Dificil: 3 {}".format(num_secreto)))
 
 if(dificuldade == 1):
     tentativa = 15
@@ -30,7 +31,7 @@ for rodada in range(1, tentativa + 1):
 
 
     if(acertou):
-        print("PARABÉNS!! Você acertou na tentativa: ", rodada)
+        print("PARABÉNS!! Você acertou na tentativa: {} e fez o total de: {} pontos".format(rodada,pontos))
         break
 
     else:
@@ -38,3 +39,7 @@ for rodada in range(1, tentativa + 1):
             print("Você chutou muito alto")
         elif(menor):
             print("Você chutou muito abaixo") 
+        pontos_perdidos = abs(num_secreto - 20)
+        pontos = pontos - pontos_perdidos
+
+print("Fim de jogo!")
